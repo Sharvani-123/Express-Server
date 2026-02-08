746c3b66da91f1d26dd07c0f77af37d1eb311394
Sharvani <sharvani11620@gmail.com>
2026-02-08 11:30:04 +0530

    Expense DAO updated

--- src/dao/expenseDao.js
+++ src/dao/expenseDao.js
@@ -2,22 +2,27 @@ const Expense = require('../model/expense');
 
 const expenseDao={
     addExpense: async (data) => {
-        
+        const newExpense = new Expense(data);
+        return await newExpense.save();
     },
-    updateExpense: async (expenseId) => {
-
+    updateExpense: async (expenseId,updateData) => {
+        return await Expense.findByIdAndUpdate(
+            expenseId,
+            updateData,
+            {new:true}
+        );
     },
     deleteExpense: async(expenseId) => {
-
+        return await Expense.findByIdAndDelete(expenseId);
     },
     getExpensesByGroup: async (groupId) => {
-        
+        return await Expense.find({ groupId });
     },
     getExpense: async (expenseId) => {
-        
+        return await Expense.findById(expenseId);
     },
-    markGroupAsSettled: async (groupId) => {
-        
+    updateExpensesByGroup: async (groupId, updateData) => {
+        return await Expense.updateMany({ groupId }, updateData);
     }
 };
 
